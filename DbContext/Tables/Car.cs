c9675660c6b951d77bb29e7b59c0bf16f0e522c2
Sahib Aliyev <sahib.aliyev.2002@gmail.com>
2023-04-21 19:25:39 +0400

    Test

---
+++ DbContext/Tables/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowRoom.DbContext.Tables
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+    }
+}
